7a6d84680a64be42b9cf530c824d2ceca5337272
yuribigon <yuri.bigon@gmail.com>
2024-01-31 15:14:42 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
       <div className="px-5 pt-5 ">
         <h2 className="text-xl font-bold">Olá, Yuri!</h2>
         <p className="text-sm">
@@ -14,6 +16,9 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+      <div className="px-5 mt-5">
+        <Search />
       </div>
     </div>
   );
