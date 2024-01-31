2df0b7a569296775effbbc9e78e0472fb10c1593
yuribigon <yuri.bigon@gmail.com>
2024-01-31 14:51:27 -0300

    feat: welcome message component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <div className="px-5 pt-5 ">
+        <h2 className="text-xl font-bold">Olá, Yuri!</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' d 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
