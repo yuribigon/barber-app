054ab39bf1fd315a8a2abbf3196c605da3c194cc
yuribigon <yuri.bigon@gmail.com>
2024-02-03 15:59:50 -0300

    feat: add search function on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -65,7 +65,9 @@ export default async function Home() {
 
           <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
             {barbershops.map((barbershop) => (
-              <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+              <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+                <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+              </div>
             ))}
           </div>
       </div>
@@ -75,7 +77,9 @@ export default async function Home() {
 
           <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
             {barbershops.map((barbershop) => (
+              <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
               <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+            </div>
             ))}
           </div>
       </div>
