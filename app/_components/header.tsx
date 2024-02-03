2639db4efd071ac8454776ab631a69e500f581a9
yuribigon <yuri.bigon@gmail.com>
2024-02-03 16:10:02 -0300

    feat: put header and footer tag on project

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -9,26 +9,29 @@ import SideMenu from "./side-menu";
 import Link from "next/link";
 
 const Header = () => {
-    
-    return (
-        <Card>
-            <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Link href="/">
-                    <Image src='/logo.png' alt="logo fsw" height={22} width={120} />
-                </Link>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button variant="outline" size="icon">
-                            <MenuIcon />
-                        </Button>
-                    </SheetTrigger>
 
-                    <SheetContent className="p-0">
-                        <SideMenu />
-                    </SheetContent>
-                </Sheet>
-            </CardContent>
-        </Card>
+    return (
+        <header>
+            <Card>
+                <CardContent className="p-5 justify-between items-center flex flex-row">
+                    <Link href="/">
+                        <Image src='/logo.png' alt="logo fsw" height={22} width={120} />
+                    </Link>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <MenuIcon />
+                            </Button>
+                        </SheetTrigger>
+
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
+
     );
 }
 
